6ce6c2becdba6ccacdc57f5c15cb6fea492729ef
darshandaru8517-web <darshandaru8517@gmail.com>
2025-11-17 19:13:36 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(const VarietasApp());
+}
+
+class VarietasApp extends StatelessWidget {
+  const VarietasApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Varietas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
